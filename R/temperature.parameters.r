620e8ea7238164c59839c22d2ac72b4bfcf3fbf0
Jae S Choi <choijaeseok@gmail.com>
2017-01-22 15:09:53 -0400

    remove bottom.gridded

--- R/temperature.parameters.r
+++ R/temperature.parameters.r
@@ -52,7 +52,7 @@ temperature.parameters = function( p=NULL, current.year=NULL, DS="default" ) {
 
     p$boundary = TRUE 
     p$depth.filter = 0 # depth (m) stats locations with elevation > 0 m as being on land (and so ignore)
-    p$lbm_nonconvexhull_alpha = 25  # radius in distance units (km) to use for determining boundaries
+    p$lbm_nonconvexhull_alpha = 30  # radius in distance units (km) to use for determining boundaries
     p$lbm_noise = 0.1  # distance units for eps noise to permit mesh gen for boundaries
     p$lbm_quantile_bounds = c(0.01, 0.99) # remove these extremes in interpolations
     
