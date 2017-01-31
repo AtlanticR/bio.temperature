47e70116dc663dbf0b3d13c2272ee5febbb14b70
Jae S Choi <choijaeseok@gmail.com>
2017-01-31 15:08:04 -0400

    fix plotting method

--- R/temperature.map.r
+++ R/temperature.map.r
@@ -12,29 +12,16 @@ temperature.map = function( ip=NULL, p=NULL, type="all", vname=NULL ) {
 
   if ( type=="all" ) {
 
-    for ( gr in p$spatial.domain.subareas ) {
+    allgrids = unique(c( p$spatial.domain.subareas, p$spatial.domain) )
+    for ( gr in allgrids ) {
       print (gr)
       p1 = spatial_parameters(  p=p, type= gr )
       p1 = make.list( list( yrs=p1$yrs), Y=p1 )
       temperature.map( p=p1, type="lbm.stats" ) # no parallel option .. just a few
       temperature.map( p=p1, type="climatology" ) # no parallel option .. just a few
-    }
-
-    # all seasonal predicted means
-    for ( gr in p$spatial.domain.subareas ) {
-      print (gr)
-      p1 = spatial_parameters( p=p, type= gr )
-      p1 = make.list( list( yrs=p1$yrs), Y=p1 )
-      parallel.run( temperature.map, p=p1, type="seasonal" )
-    }
-
-    # bottom.statistics.annual
-    for ( gr in p$spatial.domain.subareas ) {
-      print (gr)
-      p1 = spatial_parameters( p=p, type= gr )
-      p1 = make.list( list( yrs=p1$yrs), Y=p1 )
-      for ( bs in p$bstats )
-        parallel.run( temperature.map, p=p1, type="annual", vname=bs )
+      parallel.run( temperature.map, p=p1, type="seasonal" ) # all seasonal predicted means
+      for ( bs in p$bstats ) {
+        parallel.run( temperature.map, p=p1, type="annual", vname=bs ) # bottom.statistics.annual
       }
     }
 
